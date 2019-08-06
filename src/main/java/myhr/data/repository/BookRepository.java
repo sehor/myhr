a11dee7681df29cc43e8992f2df6ae96a5a77157
sehor <714534524@qq.com>
2019-08-06 18:19:06 +0800

    Rest test1

--- src/main/java/myhr/data/repository/BookRepository.java
+++ src/main/java/myhr/data/repository/BookRepository.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import myhr.data.domain.Book;
 
 @Repository
+@RepositoryRestResource
 public interface BookRepository extends JpaRepository<Book, Integer>,BookDataHelper {
 
 	List<Book> findByAuthorLike(String author);
